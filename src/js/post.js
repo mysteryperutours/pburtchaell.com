d9a23221cbc07d2168cc17e0a1908cefff49acfb
Patrick Burtchaell <patrick@pburtchaell.com>
2013-12-05 16:31:15 -0600

    add mailchimp AJAX code

--- src/js/post.js
+++ src/js/post.js
@@ -5,3 +5,35 @@
  * Copyright (c) 2013 PBDVA, Ltd.
  * Licensed under the MIT license, pburtchaell.com/license 
  */
+ 
+ $(document).ready( function () {
+    // I only have one form on the page but you can be more specific if need be.
+    var $form = $('form');
+
+    if ( $form.length > 0 ) {
+        $('form input[type="submit"]').bind('click', function ( event ) {
+            if ( event ) event.preventDefault();
+            // validate_input() is a validation function I wrote, you'll have to substitute this with your own.
+            if ( validate_input($form) ) { register($form); }
+        });
+    }
+});
+
+function register($form) {
+    $.ajax({
+        type: $form.attr('method'),
+        url: $form.attr('action'),
+        data: $form.serialize(),
+        cache       : false,
+        dataType    : 'json',
+        contentType: "application/json; charset=utf-8",
+        error       : function(err) { alert("Well this just plan bad. We could not connect; please try again later."); },
+        success     : function(data) {
+            if (data.result != "success") {
+                // Something went wrong, do something to notify the user. maybe alert(data.msg);
+            } else {
+                // It worked, carry on...
+            }
+        }
+    });
+}
