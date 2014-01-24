a32c6a69a5f3f9a487d487ad57d93268c3ac1df7
Patrick Burtchaell <patrick@pburtchaell.com>
2014-01-23 22:26:52 -0600

    Remove unused 'post.js' file

--- src/js/post.js
+++
@@ -1,7 +0,0 @@
-/* Website of Patrick Burtchaell - User interface designer, videographer, & outdoorsman
- * http://www.pburtchaell.com/ - http://twitter.com/pburtchaell
- *
- *
- * Copyright (c) 2013 PBDVA, Ltd.
- * Licensed under the MIT license, pburtchaell.com/license 
- */
