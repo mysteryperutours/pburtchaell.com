1e82d1d98ae870dce324653c2014844b41c879f0
Patrick Burtchaell <patrick@pburtchaell.com>
2014-07-18 18:26:12 -0500

    Implement Browserify JS
    - browser.js
    - link.js
    - theme.js
    - Delete classie.js (using classList API)
    - Fix initialize on page load and move into app.js
    - Delete components folder and move JS to modules

--- src/js/modules/browser.js
+++ src/js/modules/browser.js
@@ -1,4 +1,4 @@
-var browser = {
+module.exports = browser = {
   
   getElementsByClassName: function(search) {
     var d = document, elements, pattern, i, results = [];
@@ -24,17 +24,9 @@ var browser = {
   },
     
   init: function () {
-    var string = '<div style="text-align:center;height:100%;width:100%;position:fixed;background: #fff;z-index:10;padding-top:100px;">Your browser is not supported. <a href=http://outdatedbrowser.com/>Please upgrade</a>.</div>';
+    var string = '<div style="text-align:center;height:auto;width:100%;min-height:32px;position:relaztive;background:#efefcb;color:#bfbfa6;z-index:10;padding:25px;">Your browser is not supported. <a href=http://outdatedbrowser.com/>Please upgrade</a>.</div>';
     var el = this.getElementsByClassName('not-supported')[0];
-    
     el.innerHTML = string;
   }
 
-};
-
-
-if (feature.addEventListener && feature.querySelectorAll) {
-  
-} else {
-  noSupport.init();
 };
--- src/js/utils/feature.js
+++ src/js/utils/feature.js
@@ -3,7 +3,7 @@
  * - http://responsivenews.co.uk/post/18948466399/cutting-the-mustard
  * - http://www.phrases.org.uk/meanings/cut-the-mustard.html
  */
-var feature = {
+module.exports = feature = {
   addEventListener : !!window.addEventListener,
   querySelectorAll : !!document.querySelectorAll,
 };
