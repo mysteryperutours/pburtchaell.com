b16b253843b07b764d62868a40eff2b92cfc961d
Patrick Burtchaell <patrick@pburtchaell.com>
2014-09-05 11:59:08 -0500

    Remove extra "module.exports" from browserify.js

--- gulp/tasks/browserify.js
+++ gulp/tasks/browserify.js
@@ -1,18 +1,14 @@
 var gulp = require('gulp');
 var browserify = require('browserify');
+var plumber = require('gulp-plumber');
 var source = require('vinyl-source-stream');
 var opt = require('../options.json');
 
 module.exports = function () {
-  browserify(opt.src + '/js/app.js')
-  .bundle()
-  .pipe(source('components.js'))
-  .pipe(gulp.dest(opt.dest + '/assets/js/'));
-};
-
-module.exports = function () {
   browserify(opt.src + '/js/modules/work/app.js')
   .bundle()
+  .pipe(plumber())
+  .on('error', gutil.log)
   .pipe(source('app.js'))
   .pipe(gulp.dest(opt.dest + '/assets/js/apps/work/'));
 };
