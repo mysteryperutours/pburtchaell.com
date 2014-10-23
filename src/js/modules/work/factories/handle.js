4a55276888f961325e42413320ba4b9431350831
Patrick Burtchaell <patrick@pburtchaell.com>
2014-10-23 08:02:37 -0500

    Delete `callback()` function

--- src/js/modules/work/factories/handle.js
+++ src/js/modules/work/factories/handle.js
@@ -2,11 +2,9 @@ module.exports = function () {
   return { 
     InternalError: function (msg, callback) {
       throw new Error(msg);
-      callback();
     },
     ExternalError: function (msg, callback) {
       throw new Error(msg);
-      callback();
     }
   }
 }
