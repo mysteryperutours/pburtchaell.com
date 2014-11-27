80a5b0f8f9abbceddfd867e112353d24cda469b3
Patrick Burtchaell <patrick@pburtchaell.com>
2014-11-26 18:26:42 -0600

    Add "gulp/tasks/scripts.js"

---
+++ gulp/tasks/scripts.js
@@ -0,0 +1,18 @@
+var gulp = require('gulp');
+var browserify = require('browserify');
+var plumber = require('gulp-plumber');
+var gutil = require('gulp-util');
+var rev = require('gulp-rev');
+var buffer = require('gulp-buffer');
+var source = require('vinyl-source-stream');
+var opt = require('../options.json');
+
+module.exports = function () {
+  browserify(opt.src + '/js/app.js')
+    .bundle()
+    .pipe(plumber())
+    .on('error', gutil.log)
+    .pipe(source('app.js'))
+    .pipe(buffer())
+    .pipe(gulp.dest(opt.dest + '/js/'))
+};
