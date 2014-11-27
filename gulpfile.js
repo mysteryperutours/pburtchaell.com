426f0f6058edd7d9ded085ed6a1351accbdfb190
Patrick Burtchaell <patrick@pburtchaell.com>
2014-11-26 18:44:59 -0600

    Remove comments from `gulpfile.js`

--- gulpfile.js
+++ gulpfile.js
@@ -3,29 +3,14 @@ var tasks = require('./gulp/tasks');
 var tests = require('./gulp/tests');
 var utils = require('./gulp/utils');
 
-/** 
- * Tasks
- */
 gulp.task('styles', tasks.styles);
 gulp.task('scripts', tasks.scripts);
 gulp.task('copy', tasks.copy);
 gulp.task('build',['styles','scripts','copy']);
-
-/** 
- * Tests
- */
 gulp.task('jshint', tests.jshint);
 gulp.task('recess', tests.recess);
 gulp.task('test', ['recess','jshint']);
-
-/**
- * Utilities
- */
 gulp.task('clean', utils.clean);
 gulp.task('deploy', utils.deploy);
 gulp.task('watch', utils.watch);
-
-/**
- * Default
- */
 gulp.task('default', ['build']);
