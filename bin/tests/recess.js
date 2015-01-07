bdbd65e65bbef2cf6bbc2144f42c7019211ee7e7
Patrick Burtchaell <patrick@pburtchaell.com>
2015-01-07 16:23:26 -0600

    Delete old CSS/LESS tasks from gulp

--- bin/tests/recess.js
+++
@@ -1,9 +0,0 @@
-var gulp = require('gulp');
-var recess = require('gulp-recess');
-var opt = require('../options.json');
-
-module.exports = function (callback) {
-  return gulp.src(opt.src + '/less/**/*.less')
-    .pipe(recess())
-    .pipe(recess.reporter());
-};
