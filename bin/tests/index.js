1806821a557a6fc5c95ae23be540ead289fe1ad7
Patrick Burtchaell <patrick@pburtchaell.com>
2015-01-08 11:51:47 -0600

    Remove jshint

--- bin/tests/index.js
+++
@@ -1,1 +0,0 @@
-exports.jshint = require('./jshint');
--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,5 @@
 var gulp = require('gulp-help')(require('gulp'));
 var tasks = require('./bin/tasks');
-var tests = require('./bin/tests');
 var utils = require('./bin/utils');
 
 gulp.cache = {};
@@ -11,8 +10,6 @@ gulp.cache.opt = {
 
 gulp.task('bundle', false, tasks.bundle);
 gulp.task('build', 'Bundle scripts and compile the build.', ['bundle']);
-gulp.task('jshint', false, tests.jshint);
-gulp.task('test', 'Test scripts for errors.', ['jshint']);
 gulp.task('clean', 'Delete the build folder.', utils.clean);
 gulp.task('deploy','Deploy the build to S3.', utils.deploy);
 gulp.task('watch', false, utils.watch);
