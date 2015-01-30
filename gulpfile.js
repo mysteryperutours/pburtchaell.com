d6f8937278c74ae9cefcb4db9f571abc6b8d264e
Patrick Burtchaell <patrick@pburtchaell.com>
2015-01-30 16:54:48 -0600

    Implement ESLint

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,18 @@
 var gulp = require('gulp-help')(require('gulp'));
 var tasks = require('./bin/tasks');
+var tests = require('./bin/tests');
 var utils = require('./bin/utils');
 
 gulp.cache = {};
 gulp.cache.opt = {
   src: './source',
-  dest: './dest'
+  dest: './dest',
+  tests: './bin/tests/'
 };
 
 gulp.task('bundle', false, tasks.bundle);
-gulp.task('build', 'Bundle scripts and compile the build.', ['bundle']);
+gulp.task('build', 'Bundle Javascript modules.', ['bundle']);
+gulp.task('test', 'Lint code.', tests.eslint);
 gulp.task('clean', 'Delete the build folder.', utils.clean);
 gulp.task('deploy','Deploy the build to S3.', utils.deploy);
 gulp.task('serve', 'Run a development server.', utils.server);
