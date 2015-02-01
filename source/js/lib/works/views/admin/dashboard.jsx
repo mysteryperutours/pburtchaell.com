b41cff182245f0ebc5ce275064ff23aea06c98e8
Patrick Burtchaell <patrick@pburtchaell.com>
2015-01-31 18:59:36 -0600

    Add initial dashboard logic

--- source/js/lib/works/views/admin/dashboard.jsx
+++ source/js/lib/works/views/admin/dashboard.jsx
@@ -2,21 +2,109 @@
 
 var React = require('react');
 
+var Input = require('react-input');
+
+//var ProjectActionCreator = require('../../actions/project');
+
 var DashboardView = React.createClass({
+
+  /**
+   * @private
+   */
+  _reset: function (callback) {
+    try {
+      this.refs.name.reset();
+      this.refs.date.reset();
+      this.refs.type.reset();
+      this.refs.image.reset();
+      callback(null); //done
+    } catch (error) {
+      callback(error);
+    }
+  },
+
+    /**
+   * @private
+   */
+  _notify: function () {
+    this.refs.notification.open();
+  },
+
+  /**
+   * @private
+   */
+  _submit: function (event) {
+
+    event.preventDefault();
+
+    var name = this.refs.name;
+    var date = this.refs.date;
+    var type = this.refs.type;
+
+    ProjectActionCreator.create('', function () {
+
+      // Set the state
+      this.setState({
+        notification: ''
+      });
+
+      // Notify that the form was created
+      this._notify('', function () {
+        return;
+      });
+
+      // Reset the forms
+      this._reset();
+
+    }.bind(this));
+
+    return;
+
+  },
+
+  componentDidMount: function () {
+    this.refs.name.focus();
+  },
+
   render: function() {
     return (
       <div className="row">
         <form className="col col-l-12">
           <h1>New Project</h1>
-          <input placeholder="Name"/>
-          <input placeholder="Date"/>
-          <input placeholder="Type"/>
-          <input placeholder="Image"/>
-          <input type="file"/>
+          <Input
+            ref="name"
+            placeholder="Name"
+            label="A name for the project"
+            disabled={false}
+            type="text"
+          />
+          <Input
+            ref="date"
+            placeholder="test"
+            label="enter some text"
+            disabled={false}
+            type="text"
+          />
+          <Input
+            ref="type"
+            placeholder="test"
+            label="enter some text"
+            disabled={false}
+            type="text"
+          />
+          <Input
+            ref="type"
+            placeholder="test"
+            label="enter some text"
+            disabled={true}
+            type="text"
+          />
+          <button type="submit" onClick={this._submit}/>
         </form>
       </div>
     )
   }
+
 });
 
 module.exports = DashboardView;
