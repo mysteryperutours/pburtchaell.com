1d7fb9f2ee4f1c37d208c57cb7485713a0482965
Patrick Burtchaell <patrick@pburtchaell.com>
2015-02-16 23:30:31 -0600

    Update common module

--- web_modules/common/index.js
+++ web_modules/common/index.js
@@ -1,40 +1,35 @@
-import browser = 'utils/browser';
+import browser from 'utils/browser';
 
 /**
- * Cut the mustard and test for browser support (IE9+ required):
- *   - http://responsivenews.co.uk/post/18948466399/cutting-the-mustard
- *   - http://www.phrases.org.uk/meanings/cut-the-mustard.html
- *
- * If these features are supported, then the application will initialize,
- * if the features are not supported, then the application will not.
+ * Cut the mustard and test for browser support:
+ * http://responsivenews.co.uk/post/18948466399/cutting-the-mustard
  */
 if (!!window.addEventListener && !!document.querySelectorAll) {
+
   document.addEventListener('DOMContentLoaded', function () {
 
     browser('supported', function (error) {
       if (error && development) {
-        console.warn(error);
+        console.warn('Browser support:', error);
+        return;
       } else if (development) {
-        console.log(true);
+        console.log('Browser support:', true);
+        return true;
       }
     });
 
-    require.ensure(['./lib/link','./lib/images'], function () {
-      var link = require('./lib/link');
-      var images = require('./lib/images');
-    });
-
-    if (window.post === true) {
-      /*require.ensure(['./lib/post'], function () {
-        require('./lib/post');
-      });*/
-    }
-
   }, false);
+
 } else {
+
   browser('unsupported', function (error) {
     if (error && development) {
-      console.warn(error);
+      console.warn('Browser support:', error);
+      return;
+    } else if (development) {
+      console.log('Browser support:', false);
+      return false;
     }
   });
+
 };
