2d79303898b4cc2fa48fe2525ac864f33371ec0e
Patrick Burtchaell <patrick@pburtchaell.com>
2015-03-06 16:24:02 -0500

    Use ES6 "import" for route handlers

--- web_modules/works/routes.js
+++ web_modules/works/routes.js
@@ -2,28 +2,25 @@ import React from 'react';
 import { Route, NotFoundRoute, DefaultRoute, RouteHandler } from 'react-router';
 
 /**
- * @object Handlers
- * @description The views of the application.
+ * Import the route handlers (views) of the application.
  */
-var Handlers = {
-  index: require('./views/index'),
-  projects: require('./views/projects'),
-  signin: require('./views/admin/signin'),
-  dashboard: require('./views/admin/dashboard'),
-  error: require('./views/error')
-};
+import index from 'works/views/index';
+import projects from 'works/views/projects';
+import signin from 'works/views/admin/signin';
+import dashboard from 'works/views/admin/dashboard';
+import error from 'works/views/error';
 
 /**
  * @object Routes
- * @description The routes of the application
+ * @description The routes of the application.
  */
 let Routes = (
-  <Route name="app" path="/" handler={Handlers.index}>
-    <DefaultRoute handler={Handlers.signin} />
-    <Route name="dashboard" path="/admin" handler={Handlers.dashboard} />
-    <Route name="signin" path="/signin" handler={Handlers.signin} />
-    <Route name="projects" path="/projects" handler={Handlers.projects} />
-    <NotFoundRoute handler={Handlers.error} />
+  <Route name="app" path="/" handler={index}>
+    <DefaultRoute handler={signin} />
+    <Route name="dashboard" path="/admin" handler={dashboard} />
+    <Route name="signin" path="/signin" handler={signin} />
+    <Route name="projects" path="/projects" handler={projects} />
+    <NotFoundRoute handler={error} />
   </Route>
 );
 
