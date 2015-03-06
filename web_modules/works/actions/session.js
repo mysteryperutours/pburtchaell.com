aa2f735a07e4046b62139d545f515a27681b9d08
Patrick Burtchaell <patrick@pburtchaell.com>
2015-03-06 14:41:51 -0500

    Create session actions

--- web_modules/works/actions/session.js
+++ web_modules/works/actions/session.js
@@ -1,4 +1,30 @@
+import { Action } from 'flummox';
+import Firebase from 'firebase';
 
+export default class SessionActions extends Action {
 
+  constructor(flux) {
+    super();
+    this.reference = new Firebase('http://pburtchaell.firebaseio.com/data/session/');
+  }
 
+  /**
+   * @function createSession
+   * @description Create a new session for a user.
+   * @param {string} username
+   * @param {string} password
+   */
+  createSession(username, password) {
+    let response = this.reference.login(username, password);
+    return sid;
+  }
 
+  /**
+   * @function terminateSession
+   * @description Terminate the current session.
+   */
+  terminateSession() {
+    return true;
+  }
+
+}
