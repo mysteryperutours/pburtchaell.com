61fc66d960860a545a74e80bf9d2225ab8441ebf
Patrick Burtchaell <patrick@pburtchaell.com>
2015-03-06 14:42:19 -0500

    Create flux class

---
+++ web_modules/works/flux.js
@@ -0,0 +1,20 @@
+import { Flummox } from 'flummox';
+
+import ProjectActions from 'works/actions/project';
+import SessionActions from 'works/actions/session';
+import UserActions from 'works/actions/user';
+import ProjectStores from 'works/stores/project';
+import SessionStores from 'works/stores/session';
+import UserStores from 'works/stores/user';
+
+export default class Flux extends Flummox {
+  constructor() {
+    super();
+    this.createActions('project', ProjectActions);
+    this.createActions('session', SessionActions);
+    this.createActions('user', UserActions);
+    this.createStore('project', ProjectStore, this);
+    this.createStore('session', SessionStore, this);
+    this.createStore('user', UserStore, this);
+  }
+}
