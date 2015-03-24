2b7a495bac8619822bdcc8ac3db9e757cefd0d3c
Patrick Burtchaell <patrick@pburtchaell.com>
2015-03-23 23:22:30 -0500

    Add window.fetch pollyfill

--- web_modules/works/init.js
+++ web_modules/works/init.js
@@ -1,3 +1,4 @@
 import React from 'react';
+import 'babel/polyfill'; // ECMAScipt 6 and ECMAScript 7 pollyfills
+import 'whatwg-fetch'; // window.fetch API pollyfill
 window.React = React;
-import 'babel/polyfill';
