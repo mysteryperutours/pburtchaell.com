0c6531be5412a30f66bbe1fc00166efc0fee1d1c
Patrick Burtchaell <patrick@pburtchaell.com>
2015-03-28 10:37:05 -0500

    Use ES6 syntax for dashboard view

--- web_modules/works/views/admin/dashboard.js
+++ web_modules/works/views/admin/dashboard.js
@@ -3,30 +3,65 @@ import Input from 'react-input';
 import Notification from 'react-notification';
 import { Link, Redirect, Navigation } from 'react-router';
 
-let DashboardView = React.createClass({
+export default class DashboardView extends React.Component {
 
-  mixins: [Navigation],
+  constructor(props) {
 
-  /**
-   * @private
-   */
-  _reset() {
+    super(props);
+
+    let projects = [
+      {
+        id: 0,
+        name: 'Foo',
+        description: 'This is foo',
+        type: 'Video',
+        date: '1-2-2014'
+      },
+      {
+        id: 1,
+        name: 'Bar',
+        description: 'This is bar',
+        type: 'Design',
+        date: '2-3-2014'
+      },
+      {
+        id: 2,
+        name: 'Baz',
+        description: 'This is baz',
+        type: 'Web',
+        date: '3-4-2014'
+      }
+    ];
+
+    // Get intial component state
+    this.state = {
+      projects: projects,
+      current: projects[0]
+    };
+
+    // Component methods
+    this.resetForm = this.resetForm.bind(this);
+    this.showNotification = this.showNotification.bind(this);
+    this.changeProject = this.changeProject.bind(this);
+    this.getProjectStyles = this.getProjectStyles.bind(this);
+
+    // Component event handlers
+    this.handleFormSubmission = this.handleFormSubmission.bind(this);
+    this.handleProjectEditor = this.handleProjectEditor.bind(this);
+
+  }
+
+  resetForm() {
     this.name.reset();
     this.date.reset();
     this.type.reset();
     this.image.reset();
-  },
+  }
 
-  /**
-   * @private
-   */
-  _notify() {
+  showNotification() {
     this.notification.show();
-  },
+  }
 
-  /**
-   * @private
-   */
   handleFormSubmission(event) {
 
     event.preventDefault();
@@ -36,53 +71,27 @@ let DashboardView = React.createClass({
     let date = this.date.value();
     let type = this.type.value();
 
-    this._notify();
+    this.showNotification();
 
-  },
+  }
 
-  handleProjectEditor() {},
+  changeProject(id) {
+    var newProject = this.state.projects[id];
+    this.setState({
+      current: newProject
+    });
+  }
 
-  componentDidMount() {
-    this.name.focus();
-  },
+  handleProjectEditor(id, event) {
+    event.preventDefault();
+    this.changeProject(id);
+  }
 
   getProjectStyles() {
     return {
       background: `url(https://snap-photos.s3.amazonaws.com/img-thumbs/960w/JTTWFDU1S5.jpg)`
     };
-  },
-
-  getInitialState() {
-
-    let projects = [
-      {
-        id: 1,
-        name: 'Foo',
-        description: 'This is foo',
-        type: 'Video',
-        date: '1-2-2014'
-      },
-      {
-        id: 2,
-        name: 'Bar',
-        description: 'This is bar',
-        type: 'Design',
-        date: '2-3-2014'
-      },
-      {
-        id: 3,
-        name: 'Baz',
-        description: 'This is baz',
-        type: 'Web',
-        date: '3-4-2014'
-      }
-    ];
-
-    return {
-      projects: projects,
-      current: projects[0]
-    };
-  },
+  }
 
   render() {
     return (
@@ -91,39 +100,45 @@ let DashboardView = React.createClass({
         <section className="dashboard-items-slider">
           <h4><small>All Projects</small></h4>
           <div className="slider-wrapper">
-            {this.state.projects.map(project => (
+            {this.state.projects.map(project =>
               <div
                 key={project.id}
                 style={this.getProjectStyles(project.id)}
                 className="items-slider-item"
-                onClick={this.handleProjectEditor}>
+                onClick={this.handleProjectEditor.bind(null, project.id)}>
                 <span className="items-slider-item-title">{project.name}</span>
               </div>
-            ))}
+            )}
           </div>
         </section>
+
+        // @TODO: listen to props and rerender on change. there is some bug in react-input
 
         <section className="default-content">
           <div className="row">
             <form className="col col-l-12">
+            {this.state.current.name}
+            {this.state.current.date}
+            {this.state.current.type}
+            {this.state.current.description}
               <Input
                 ref={(c) => this.name = c}
                 label="Project name"
-                value={this.state.current.name}
+                defaultValue={this.state.current.name}
                 type="text" />
               <Input
                 ref={(c) => this.date = c}
                 label="Project Date"
-                value={this.state.current.date}
+                defaultValue={this.state.current.date}
                 type="text" />
               <Input
                 ref={(c) => this.type = c}
                 label="Project Type"
-                value={this.state.current.type}
+                defaultValue={this.state.current.type}
                 type="text" />
               <Input
                 ref={(c) => this.description = c}
-                value={this.state.current.description}
+                defaultValue={this.state.current.description}
                 label="Project Description"
                 type="text" />
               <button className="btn" type="submit" onClick={this.handleFormSubmission}>Create project</button>
@@ -136,6 +151,4 @@ let DashboardView = React.createClass({
     );
   }
 
-});
-
-export default DashboardView;
+}
