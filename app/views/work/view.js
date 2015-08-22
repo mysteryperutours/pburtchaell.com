fcee88aa4f91e2b14fc6db4c113e4548dbeb429a
Patrick Burtchaell <patrick@pburtchaell.com>
2015-08-21 22:37:07 -0500

    Hmmmm

---
+++ app/views/work/view.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import View from 'components/layout/view';
+import Section from 'components/layout/section';
+
+export default class WorkView extends Component {
+  render() {
+    return (
+      <View>
+          <h1 className="text-align-center">Design & Code</h1>
+          <p>I'm working on getting some of my </p>
+      </View>
+    );
+  }
+}
