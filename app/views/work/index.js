9634f3e5c71f4bcf949abc27d62c9cd9e3780934
Patrick Burtchaell <patrick@pburtchaell.com>
2015-09-18 02:12:07 -0500

    Update work routes

--- app/views/work/index.js
+++ app/views/work/index.js
@@ -4,7 +4,9 @@ export default {
   childRoutes: [
     require('./children/lawnchair'),
     require('./children/loyola'),
+    require('./children/oa'),
     require('./children/peach'),
+    require('./children/personal'),
     require('./children/scouter'),
     require('./children/segment')
   ],
