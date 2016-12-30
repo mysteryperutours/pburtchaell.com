53cf8eb3388c78f6551a83e2d91f9727c5b12256
Patrick Burtchaell <patrick@pburtchaell.com>
2016-12-30 16:07:10 -0600

    Update index route content

--- app/routes/index/component.js
+++ app/routes/index/component.js
@@ -1,6 +1,11 @@
 // @flow
-import React, { Component, Element } from 'react';
+import React, { Component, Element, createElement } from 'react';
 import RouteContainer from '../../components/routeContainer';
+import Text, { types } from '../../components/text';
+import Row from '../../components/row';
+import Column from '../../components/column';
+import ListView from '../../components/listView';
+import requestHandler from '../../support/requestHandler';
 
 type Props = {
   children: Element<*>
@@ -11,20 +16,74 @@ class IndexRoute extends Component {
     super(props, context);
 
     this.state = {
-      foo: 'bar'
+      isPending: true
     };
   }
 
   state: {
-    foo: 'foo' | 'bar'
+    isPending: boolean
   };
+
+  componentWillMount() {
+    requestHandler.call(this, `${DATABASE_URL}/projects.json`);
+  }
 
   props: Props;
 
-  render() {
+  render(): RouteContainer {
+    const { isPending, data } = this.state;
+
     return (
-      <RouteContainer header={false}>
-        Hello, world!
+      <RouteContainer>
+        <Row>
+          <Text
+            type={types.HEADER_1}
+            style={{
+              marginTop: '1rem',
+              marginBottom: '3rem'
+            }}
+          >Patrick Burtchaell is a product design intern at Facebook, studying design at Loyola University New Orleans.</Text>
+        </Row>
+        <Row>
+          <Column size={8}>
+            <Text
+              type={types.HEADER_2}
+              style={{
+                marginBottom: '3rem'
+              }}
+            >Previously, he worked freelance and at <a href="">Fixt</a>.</Text>
+          </Column>
+        </Row>
+        <Row>
+          <Text
+            type={types.HEADER_3}
+            style={{
+              marginBottom: '2rem'
+            }}
+          >Selected Works</Text>
+        </Row>
+        {isPending ? null : (
+          <ListView
+            itemStyle={{
+              height: '30rem',
+              borderRadius: '1px',
+              backgroundSize: 'cover'
+            }}
+            items={Object.keys(data).map((key) => {
+              const { meta } = data[key];
+              const { date } = meta;
+
+              return ({
+                id: meta.id,
+                title: meta.title,
+                linkTo: `/work/${date.year}/${date.month}/${meta.pathname}`,
+                style: {
+                  backgroundImage: `url(${meta.coverImage.url})`
+                }
+              });
+            })}
+          />
+        )}
       </RouteContainer>
     );
   }
