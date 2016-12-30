6fd02bdb36b26b1dc6f9a58e3c5affc767c4e653
Patrick Burtchaell <patrick@pburtchaell.com>
2016-12-30 12:55:31 -0600

    Implement column and row components

--- app/components/column/index.js
+++ app/components/column/index.js
@@ -1,21 +1,50 @@
 // @flow
-import React, { Element } from 'react';
+import { createElement, Element } from 'react';
 import './styles.css';
 
 type Props = {
-  className: string,
   children?: any, // @TODO: Use Element<*> once facebook/flow #1964 is closed
-  size: string
+  className?: string,
+  elementType: string,
+  offset?: number,
+  size: 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12,
+  style?: Object
 }
 
-const Column = (props: Props): Element<*> => {
-  const classNames = ['column', props.className, props.size];
+/**
+ * The Column function can be used to create a column in the grid layout. This
+ * element is generally a child of a Row element. There are 12 columns in the
+ * grid. Using the `size` property, the width of the column can be set to any
+ * value between 1 and 12. By default, the Column will render at full width.
+ */
+const Column = ({ size, children, className, elementType, ...props }: Props): Element<*> => {
+  // The percentage width of the column on a grid 12 columns wide
+  const columnSize: number = (100 / 12);
+  const columnWidth: number = columnSize * size;
+  const columnClassName: string = 'column';
 
-  return (
-    <div className={classNames.join(' ')}>
-      {props.children}
-    </div>
+  return createElement(
+    elementType,
+    {
+      ...props,
+      className: `${columnClassName} ${columnClassName}-${size}${className ? ' ' + className : ''}`,
+      style: Object.assign({
+
+        // @TODO: Add support for IE
+        flexBasis: `${columnWidth}%`,
+        maxWidth: `${columnWidth}%`
+      }, props.offset ? {
+        marginLeft: `${columnSize * props.offset}%`
+      } : {}, props.style)
+    },
+    children
   );
+};
+
+Column.defaultProps = {
+  elementType: 'div',
+  size: 12, // Render at full width by default
+  style: {}
 };
 
 export default Column;
--- app/components/row/index.js
+++ app/components/row/index.js
@@ -1,22 +1,52 @@
 // @flow
-import React, { Element } from 'react';
+import { createElement, Element } from 'react';
+import Column from '../column';
 import './styles.css';
 
 type Props = {
-  className: string,
-  children?: any // @TODO: Use Element<*> once facebook/flow #1964 is closed
+  className?: string,
+  children?: any, // @TODO: Use Element<*> once facebook/flow #1964 is closed
+  defaultColumn?: boolean,
+  elementType: string
 }
 
-const Row = (props: Props): Element<*> => {
-  const classNames = ['row', props.className];
+/**
+ * The Row function can be used to create a new row for the grid layout. By
+ * default the Row will render as a `div` element, but using the `elementType`
+ * property the Row can be customized to render as any HTML element.
+ *
+ * The Row can also have a custom className, set using the `className` property.
+ */
+const Row = ({
+  className,
+  children,
+  defaultColumn,
+  elementType,
+  ...props
+}: Props): Element<*> => {
+  // Support rendering a single column by default
+  function renderRowChild() {
+    if (!children) return null;
 
-  return (
-    <div className="row-container">
-      <div className={classNames.join(' ')}>
-        {props.children}
-      </div>
-    </div>
-  );
+    if (children.type === Column || defaultColumn === false) {
+      return children;
+    }
+
+    return createElement(Column, {}, children);
+  }
+
+  return createElement(elementType, {
+    ...props,
+    children: createElement('div', {
+      className: 'row'
+    }, renderRowChild()),
+    className: className ? `row-container ${className}` : 'row-container'
+  });
+};
+
+Row.defaultProps = {
+  defaultColumn: true,
+  elementType: 'div'
 };
 
 export default Row;
