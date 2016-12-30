de3bcc3dd57cdcb8ae4f5ba2798361a43e7d65d3
Patrick Burtchaell <patrick@pburtchaell.com>
2016-12-30 12:46:03 -0600

    Update route header to use CSS modules

--- app/components/routeHeader/index.js
+++ app/components/routeHeader/index.js
@@ -1,19 +1,53 @@
 import React, { Element } from 'react';
-
-type Props = {
-  navigationItems: array
-}
+import Column from '../../components/column';
+import Row from '../../components/row';
+import Text from '../text';
+import paths from '../../routes/paths';
+import styles from './styles.css';
 
 /**
  * @function RouteHeader
  * @description
  */
-const RouteHeader = (props: Props): Element => (
-  <div className="view-header">
-    {props.navigationItems.map(item => (
-      <div key={item.key}>{item.label}</div>
-    ))}
-  </div>
-);
+const RouteHeader = (): Element => {
+  const navigationItems = [{
+    key: 0,
+    label: 'Home',
+    linkTo: paths.INDEX
+  }, {
+    key: 1,
+    label: 'About',
+    linkTo: paths.ABOUT
+  },
+  {
+    key: 2,
+    label: 'Work',
+    linkTo: paths.WORK
+  }];
+
+  return (
+    <header className={styles.viewHeader}>
+      <Row>
+        <Column size={12}>
+          <nav className={styles.viewNavigation}>
+            {navigationItems.map(item => (
+              <Text
+                key={item.key}
+                linkTo={item.linkTo}
+                style={{
+                  fontSize: '3rem',
+                  lineHeight: '3rem',
+                  top: '2px'
+                }}
+              >
+                {item.label}
+              </Text>
+            ))}
+          </nav>
+        </Column>
+      </Row>
+    </header>
+  );
+};
 
 export default RouteHeader;
