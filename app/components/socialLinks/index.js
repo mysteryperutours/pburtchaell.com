227aef6bc1948c0a0b81546e60d4083ea6d05208
Patrick Burtchaell <patrick@pburtchaell.com>
2017-02-14 11:49:55 -0600

    Implement initial about route

--- app/components/socialLinks/index.js
+++ app/components/socialLinks/index.js
@@ -9,7 +9,10 @@ const SocialLinks = () => (
         href="https://facebook.com/pburtchaell"
         title="Patrick Burtchaell on Facebook"
       >
-        <Icon type={types.FACEBOOK} />
+        <Icon
+          type={types.FACEBOOK}
+          fill="#0063B5"
+        />
         Facebook
       </a>
     </li>
@@ -18,7 +21,10 @@ const SocialLinks = () => (
         href="https://twitter.com/pburtchaell"
         title="Patrick Burtchaell on Twitter"
       >
-        <Icon type={types.TWITTER} />
+        <Icon
+          type={types.TWITTER}
+          fill="#0063B5"
+        />
         Twitter
       </a>
     </li>
@@ -27,7 +33,10 @@ const SocialLinks = () => (
         href="https://github.com/pburtchaell"
         title="Patrick Burtchaell on GitHub"
       >
-        <Icon type={types.GITHUB} />
+        <Icon
+          type={types.GITHUB}
+          fill="#0063B5"
+        />
         GitHub
       </a>
     </li>
@@ -36,7 +45,10 @@ const SocialLinks = () => (
         href="https://dribbble.com/pburtchaell"
         title="Patrick Burtchaell on Dribbble"
       >
-        <Icon type={types.DRIBBBLE} />
+        <Icon
+          type={types.DRIBBBLE}
+          fill="#0063B5"
+        />
         Dribbble
       </a>
     </li>
--- app/routes/about/component.js
+++ app/routes/about/component.js
@@ -3,38 +3,21 @@ import React, { Element } from 'react';
 import Column from '../../components/column';
 import Text, { types } from '../../components/text';
 import Row from '../../components/row';
+import SocialLinks from '../../components/socialLinks';
 import RouteContainer from '../../components/routeContainer';
+import './styles.css';
 
-const AboutRoute = (): Element<*> => (
-  <RouteContainer>
-    <Row>
-      <Text
-        type={types.HEADER_1}
-        style={{
-          marginTop: '1rem',
-          marginBottom: '3rem'
-        }}
-      >Patrick Burtchaell is a product design intern at Facebook, studying design at Loyola University New Orleans.</Text>
-    </Row>
+const AboutRoute = (props): Element<*> => (
+  <RouteContainer {...props.route.config}>
     <Row>
       <Column size={9}>
-        <Text key="1">Currently, Patrick is studying a B.S. in Design with Honors and expects to graduate in 2018. In 2016, he worked at Facebook in Menlo Park, California as a product design intern. He will return  to Facebook in 2017.</Text>
-        <Text key="2">Previously, patrick has worked with startups around the world as a  software developer and designer. He is a cofounder and partner at Lawn Chair, a design and software development studio he founded in 2015.</Text>
+        <Text>Currently, Patrick attends Loyola University New Orleans studying a Bachelor of Science in Design with Honors and expects to graduate in 2018.</Text>
+        <Text>Patrick has worked with startups around the world as a  software developer and designer. He is a cofounder and partner at Lawn Chair, a design and software development studio he founded in 2015.</Text>
       </Column>
     </Row>
     <Row>
-      <Column size={12}>
-        <ul className="about-links-list">
-          <li key="twitter" className="about-links-list-item">
-            <a href="https://twitter.com/pburtchaell" title="Patrick Burtchaell on Twitter">Twitter</a>
-          </li>
-          <li key="dribbble" className="about-links-list-item">
-            <a href="https://dribbble.com/pburtchaell" title="Patrick Burtchaell on Dribbble">Dribbble</a>
-          </li>
-          <li key="email" className="about-links-list-item">
-            <a href="mailto:patrick@pburtchaell.com" title="Patrick Burtchaell on email">patrick@pburtchaell.com</a>
-          </li>
-        </ul>
+      <Column size={6}>
+        <SocialLinks />
       </Column>
     </Row>
   </RouteContainer>
