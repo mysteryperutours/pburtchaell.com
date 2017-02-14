b6bef36941ceb5643f4f4780d83c82b4d4048da4
Patrick Burtchaell <patrick@pburtchaell.com>
2017-02-14 12:23:50 -0600

    Implement color header and footer

--- app/components/routeContainer/index.js
+++ app/components/routeContainer/index.js
@@ -1,11 +1,17 @@
 import React, { Element } from 'react';
 import RouteFooter from '../routeFooter';
 import RouteHeader from '../routeHeader';
+import GridPreview from '../gridPreview';
+import './styles.less';
 
 type Props = {
   header: null | object | boolean,
   footer: null | boolean,
-  children: Element<*>
+  children: Element<*>,
+  isPending?: boolean,
+  color?: String,
+  textColor?: String,
+  defaultStyles: boolean
 }
 
 /**
@@ -21,14 +27,30 @@ type Props = {
 const RouteContainer = ({
   header = true,
   footer = true,
-  children
+  defaultStyles = true,
+  children,
+  ...props
 }: Props): Element => (
   <div className="route-container">
-    {!header ? null : (<RouteHeader {...header} />)}
-    <main role="main" className="route-body">
+    {!header ? null : (
+      <RouteHeader
+        color={props.color}
+        textColor={props.textColor}
+        {...header}
+      />
+    )}
+    <main
+      role="main"
+      className={`route-body ${defaultStyles ? 'route-body-default' : null}`}
+    >
       {children}
     </main>
-    {!footer ? null : (<RouteFooter />)}
+    {props.isPending || !footer ? null : (
+      <RouteFooter
+        color={props.color}
+        {...footer}
+      />
+    )}
   </div>
 );
 
