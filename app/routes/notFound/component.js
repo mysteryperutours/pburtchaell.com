a11e20b39cdc8c8ef1ef62898f31613c8e65455c
Patrick Burtchaell <patrick@pburtchaell.com>
2017-02-14 16:23:24 -0600

    Implement 404 not found route

--- app/routes/notFound/component.js
+++ app/routes/notFound/component.js
@@ -1,10 +1,16 @@
 // @flow
 import React, { Element } from 'react';
 import RouteContainer from '../../components/routeContainer';
+import Row from '../../components/row';
+import Text, { types } from '../../components/text';
 
 const NotFoundRoute = (): Element<*> => (
-  <RouteContainer header={false}>
-    Error
+  <RouteContainer>
+    <Row>
+      <Text type={types.HEADER_1}>Not Found</Text>
+      <hr />
+      <Text>Sorry, this page does not exist.</Text>
+    </Row>
   </RouteContainer>
 );
 
