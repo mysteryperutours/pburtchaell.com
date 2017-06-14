b0313c13695bb1ec5c1aa8723f035b85d2e08be0
Patrick Burtchaell <pburtchaell@fb.com>
2017-06-14 10:32:31 -0700

    Update env vars

--- webpack.config.js
+++ webpack.config.js
@@ -171,8 +171,7 @@ const config = {
   },
   plugins: [
     new webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify(NODE_ENV),
-      DATABASE_URL: JSON.stringify(DATABASE_URL)
+      'process.env.NODE_ENV': JSON.stringify(NODE_ENV)
     }),
     new HtmlWebpackPlugin({
       inject: false,
