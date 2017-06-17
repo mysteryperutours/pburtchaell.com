4e5fc85db09b872a23f41a33afcb4e2f8d6bb496
Patrick Burtchaell <pburtchaell@fb.com>
2017-06-17 16:57:28 -0700

    Update request handler to use Contentful SDK

--- app/support/requestHandler.js
+++ app/support/requestHandler.js
@@ -1,31 +1,59 @@
 // @flow
+import { createClient } from 'contentful';
 
-function requestHandler(url: string, options: Object = {}): Promise<*> {
-  return fetch(`https://${url}`, options).then((response) => {
-    if (response.status === 200) {
-      return response.json();
-    }
+var client = createClient({
+  space: 'zj3fkmar2hjx',
+  accessToken: '48e59dfaa3797f188fccd0e6238cf0bd8015d371afeb52d4c605312ed1adc3b6'
+})
 
-    throw new Error();
-  }).then(
-    (data) => {
-      if (data.data) {
-        this.setState({
-          data: data.data,
-          meta: data.meta,
-          isPending: false
-        });
-      } else {
-        this.setState({
-          data,
-          isPending: false
-        });
-      }
-    },
-    () => this.setState({
-      isRejected: true
-    })
-  );
+function requestHandler(pathname: string): Promise<*> {
+  function filterImage(image) {
+    return {
+      id: image.sys.id,
+      description: image.fields.description,
+      title: image.fields.title,
+      url: image.fields.file.url,
+      height: image.fields.file.details.image.height,
+      width: image.fields.file.details.image.width
+    };
+  }
+
+  function filterItem(item) {
+    return {
+      body: item.fields.body,
+      title: item.fields.title,
+      tags: item.fields.tags,
+      year: item.fields.year,
+      color: item.fields.color,
+      isPromoted: item.fields.isPromoted,
+      pathname: item.fields.pathname,
+      question: item.fields.question,
+      featuredImage: filterImage(item.fields.featuredImage),
+      images: item.fields.images ? item.fields.images.map(filterImage) : null,
+      id: item.sys.id,
+      createdAt: item.sys.createdAt,
+      updatedAt: item.sys.updatedAt,
+      link: item.fields.link,
+      linkTitle: item.fields.linkTitle
+    };
+  }
+
+  let clientOptions = pathname ? {
+    'content_type': '2wKn6yEnZewu2SCCkus4as',
+    'fields.pathname[match]': pathname
+  } : {
+    'content_type': '2wKn6yEnZewu2SCCkus4as',
+    'order': '-fields.year'
+  };
+
+  return client.getEntries(clientOptions).then(response => {
+    return response.items.map(filterItem);
+  }).then(data => {
+    this.setState({
+      data,
+      isPending: false
+    });
+  });
 }
 
 export default requestHandler;
