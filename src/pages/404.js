5b84e8aa460f7417793dbd84456a4386ba45d404
Patrick <patrick@pburtchaell.com>
2018-04-03 09:18:52 -0500

    Fix undefined site.metadata object

--- src/pages/404.js
+++ src/pages/404.js
@@ -5,29 +5,33 @@ import Column from '../components/Column'
 import Row from '../components/Row'
 import Text, {types as textTypes} from '../components/Text'
 
-const NotFoundPage = () => (
-  <PageContainer
-    pageTitle="Home"
-    siteTitle={site.metadata.title}
-    siteUrl={site.metadata.url}
-    description={site.metadata.description}
-    keywords={site.metadata.keywords}
-  >
-    <Row rowSize="large" paddingSize="large">
-      <Column largeSize="4" smallSize="12">
-        <Text>
-          Page Not Found
-        </Text>
-        <Text type={textTypes.SMALL} className="small-reset-margin">
-          {`Oh no! Sorry, but there's no page here.`}
-        </Text>
-        <Text>
-          <Link to="/">&#8592; Go Home</Link>
-        </Text>
-      </Column>
-    </Row>
-  </PageContainer>
-)
+const NotFoundPage = ({data}) => {
+  const {site} = data
+
+  return (
+    <PageContainer
+      pageTitle="Home"
+      siteTitle={site.metadata.title}
+      siteUrl={site.metadata.url}
+      description={site.metadata.description}
+      keywords={site.metadata.keywords}
+    >
+      <Row rowSize="large" paddingSize="large">
+        <Column largeSize="4" smallSize="12">
+          <Text>
+            Page Not Found
+          </Text>
+          <Text type={textTypes.SMALL} className="small-reset-margin">
+            {`Oh no! Sorry, but there's no page here.`}
+          </Text>
+          <Text>
+            <Link to="/">&#8592; Go Home</Link>
+          </Text>
+        </Column>
+      </Row>
+    </PageContainer>
+  )
+}
 
 export default NotFoundPage
 
