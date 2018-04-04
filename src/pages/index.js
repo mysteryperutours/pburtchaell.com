76d6b2f2a2ec0308562ce528f78efcbf33dad18a
Patrick <patrick@pburtchaell.com>
2018-04-04 14:46:09 -0500

    Add project filter to index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,4 @@
-import React, {Fragment} from 'react'
+import React, {Component, Fragment} from 'react'
 import PropTypes from 'prop-types'
 import PageContainer from '../components/PageContainer'
 import Row from '../components/Row'
@@ -6,15 +6,221 @@ import Column from '../components/Column'
 import ListViewItem from '../components/ListView'
 import Text, {types as textTypes} from '../components/Text'
 
-const CONTACTS = [
-  {label: 'Twitter', linkTo: 'https://twitter.com/pburtchaell'},
-  {label: 'Facebook', linkTo: 'https://facebook.com/pburtchaell'},
-  {label: 'GitHub', linkTo: 'https://github.com/pburtchaell'},
-]
+// Define the default filters used by the index page
+const DEFAULT_FILTER_TYPES = {
+  FEATURED: 'FEATURED',
+  DESIGN: 'Product Design',
+  SOFTWARE_DEVELOPMENT: 'Open Source',
+}
+
+/*
+ * Function: FilterTab
+ * Description: Renders a clickable tab that can be used to switch between
+ * different project categories.
+ */
+const FilterTab = (props) => {
+  const {
+    filterType,
+    filterLabel,
+    activeFilterType,
+    filterIndex,
+    onClick,
+  } = props
+
+  // Define available styles for a tab
+  const defaultStyle = {display: 'inline-block', cursor: 'pointer'}
+  const activeStyle = {color: 'blue', borderBottom: '1px solid blue'}
+  const paddedStyle = {marginLeft: '2rem'}
+
+  // Construct styles for the tab
+  const style = {
+    // Always include the default style
+    ...defaultStyle,
+    // Include active styles if the filter is active
+    ...(filterType === activeFilterType) ? activeStyle : {},
+    // Include padding if the filter is not first
+    ...(filterIndex === 0) ? {} : paddedStyle,
+  }
+
+  return (
+    <Text
+      key={filterType}
+      type={textTypes.HEADER_3}
+      className="project-tabs__tab"
+      onClick={onClick}
+      style={style}
+    >
+      {filterLabel}
+    </Text>
+  )
+}
+
+FilterTab.propTypes = {
+  filterType: PropTypes.oneOf([
+    DEFAULT_FILTER_TYPES.FEATURED,
+    DEFAULT_FILTER_TYPES.DESIGN,
+    DEFAULT_FILTER_TYPES.SOFTWARE_DEVELOPMENT,
+  ]).isRequired,
+  filterLabel: PropTypes.string.isRequired,
+  activeFilterType: PropTypes.string.isRequired,
+  filterIndex: PropTypes.number.isRequired,
+  onClick: PropTypes.func.isRequired,
+}
+
+/*
+ * Class: FilterProjects
+ * Description: Renders a filtered list of projects for the index page and a
+ * tab bar for switching the active filter.
+ */
+class FilterProjects extends Component {
+  constructor(props, context) {
+    super(props, context)
+
+    // Define the filters (used to filter the array of projects)
+    this.filterTypes = DEFAULT_FILTER_TYPES
+
+    // Define the filter tabs (used to render tabs in the UI)
+    this.filterTabs = [
+      {id: 0, label: 'Featured', type: this.filterTypes.FEATURED},
+      {id: 1, label: 'Design', type: this.filterTypes.DESIGN},
+      {id: 0, label: 'Software Development', type: this.filterTypes.SOFTWARE_DEVELOPMENT},
+    ]
+
+    // Get the initial set of filtered projects
+    const initialFilter = this.filterTypes.FEATURED
+    const initialProjects = this.filterByCategory(props.projects, initialFilter)
+
+    this.state = {
+      // The initial filter
+      activeFilterType: initialFilter,
+      // The initial set of filtered projects
+      filteredProjects: initialProjects,
+    }
+
+    this.filterByCategory = this.filterByCategory.bind(this)
+    this.handleFilter = this.handleFilter.bind(this)
+  }
+
+  /*
+   * Function: filterByCategory
+   * Description
+   */
+  filterByCategory(projects, nextFilter) {
+    return projects.edges
+      // Reduce the projects.edges[] array
+      .map(({node}) => node)
+      // Filter based off category or featured flag
+      .filter(({frontmatter}) => {
+        if (nextFilter === this.filterTypes.FEATURED) {
+          return frontmatter.featured === true
+        } else {
+          return frontmatter.category === nextFilter
+        }
+      })
+  }
+
+  /*
+   * Function: filterByCategory
+   * Description: Given the next filter, calls filterByCategory and sets the
+   * compoent state with the next array of filtered projects.
+   */
+  handleFilter(event, nextFilterType) {
+    const {activeFilterType} = this.state
+    const {projects} = this.props
+
+    event.preventDefault()
+
+    if (nextFilterType === activeFilterType) {
+      return
+    }
+
+    this.setState({
+      activeFilterType: nextFilterType,
+      filteredProjects: this.filterByCategory(projects, nextFilterType),
+    })
+  }
+
+  render() {
+    const {filterTabs} = this
+    const {activeFilterType, filteredProjects} = this.state
+
+    // Enable the tab bar to scroll on small screens
+    // Todo: there's probably a better implementation for this, but this works
+    // for the time being--until I have more time.
+    const tabContainerSyle = {
+      position: 'relative',
+      height: '4rem',
+      overflowX: 'scroll',
+    }
+
+    const tabBarStyle = {
+      position: 'absolute',
+      width: '350px',
+    }
+
+    return (
+      <Row paddingSize="large" rowSize="large">
+        <Column largeSize={12} smallSize={12}>
+          <Text type={textTypes.HEADER_2}>
+            Work
+          </Text>
+          <div className="project-tabs__container" style={tabContainerSyle}>
+            <div className="project-tabs__bar" style={tabBarStyle}>
+              {filterTabs.map(({id, type, label}, filterTabIndex) => (
+                <FilterTab
+                  key={id}
+                  filterType={type}
+                  filterLabel={label}
+                  // Get the active filter (from the component state)
+                  activeFilterType={activeFilterType}
+                  filterIndex={filterTabIndex}
+                  onClick={(event) => this.handleFilter(event, type)}
+                />
+              ))}
+            </div>
+          </div>
+        </Column>
+        {filteredProjects.map((project) => (
+          <Column
+            key={project.id}
+            largeSize={4}
+            smallSize={12}
+          >
+            <ListViewItem
+              title={project.frontmatter.title}
+              date={project.frontmatter.date}
+              path={project.frontmatter.path}
+              category={project.frontmatter.category}
+              linkTo={project.fields.slug}
+            />
+          </Column>
+        ))}
+      </Row>
+    )
+  }
+}
+
+FilterProjects.propTypes = {
+  projects: PropTypes.shape({
+    edges: PropTypes.arrayOf(PropTypes.shape({
+      node: PropTypes.shape({
+        id: PropTypes.string.isRequired,
+        fields: PropTypes.shape({
+          slug: PropTypes.string.isRequired,
+        }).isRequired,
+        frontmatter: PropTypes.shape({
+          title: PropTypes.string.isRequired,
+          category: PropTypes.string.isRequired,
+          date: PropTypes.string.isRequired,
+        }),
+      }),
+    })),
+  }),
+}
 
 /*
  * Function: IndexPage
- * Description:
+ * Description: Renders the home page for the site.
  */
 const IndexPage = ({data}) => {
   const {projects, site} = data
@@ -41,28 +247,9 @@ const IndexPage = ({data}) => {
             </small>
           </Column>
         </Row>
-        <Row paddingSize="large" rowSize="large">
-          <Column largeSize={12} smallSize={12}>
-            <Text type={textTypes.HEADER_3}>
-              Selected Work
-            </Text>
-          </Column>
-          {projects.edges.map(({node:project}) => (
-            <Column
-              key={project.id}
-              largeSize={4}
-              smallSize={12}
-            >
-              <ListViewItem
-                title={project.frontmatter.title}
-                date={project.frontmatter.date}
-                path={project.frontmatter.path}
-                category={project.frontmatter.category}
-                linkTo={project.fields.slug}
-              />
-            </Column>
-          ))}
-        </Row>
+        <FilterProjects
+          projects={projects}
+        />
       </Fragment>
     </PageContainer>
   )
@@ -109,11 +296,11 @@ export const pageQuery = graphql`
       }
     }
     projects: allMarkdownRemark(
-      limit: 10
       filter: {
-        frontmatter: { templateKey: { eq: "project" } }
-        frontmatter: { published: { eq: true } }
-        frontmatter: { featured: { eq: true } }
+        frontmatter: {
+          templateKey: { eq: "project" }
+          published: { eq: true }
+        }
       }
       sort: {
         fields: [ frontmatter___date ],
@@ -129,6 +316,7 @@ export const pageQuery = graphql`
           }
           frontmatter {
             title
+            featured
             date(formatString: "YYYY")
             category
           }
