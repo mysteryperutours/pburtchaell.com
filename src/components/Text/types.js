28d20db6709b956e66bbfc12fabcfd67efda85c0
Patrick <patrick@pburtchaell.com>
2018-04-06 16:15:30 -0500

    Remove semicolons

--- src/components/Text/types.js
+++ src/components/Text/types.js
@@ -1,5 +1,5 @@
-export const HEADER_1 = 'h1';
-export const HEADER_2 = 'h2';
-export const HEADER_3 = 'h3';
-export const BODY = 'p';
-export const SMALL = 'small';
+export const HEADER_1 = 'h1'
+export const HEADER_2 = 'h2'
+export const HEADER_3 = 'h3'
+export const BODY = 'p'
+export const SMALL = 'small'
