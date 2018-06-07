60fc88e96b2b80c9e311ac5288f06a83dcab1939
Patrick Burtchaell <patrick@pburtchaell.com>
2018-06-07 15:23:30 -0500

    Add newsletter sign up

--- src/components/Page/content.js
+++ src/components/Page/content.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Column from '../Column';
+import Text, { types as textTypes } from '../../components/Text';
 
 /*
  * Function: PageContent
@@ -13,6 +14,22 @@ const PageContent = ({ children, flexOrder }) => {
         className="project--column-1"
         dangerouslySetInnerHTML={{ __html: children }}
       />
+      <form
+        action="https://tinyletter.com/pburtchaell"
+        method="post"
+        target="popupwindow"
+        className="newsletter-form"
+        onSubmit={() => {
+          window.open('https://tinyletter.com/pburtchaell', 'popupwindow', 'scrollbars=yes,width=800,height=600');
+          return true;
+        }}
+      >
+        <Text>Subscribe to infrequent updates on projects, interests and fun stuff</Text>
+        <input required type="email" name="email" id="tlemail" placeholder="email@domain.com" />
+        <input type="hidden" value="1" name="embed" />
+        <input type="submit" value="Subscribe" />
+        <small>Powered by TinyLetter</small>
+      </form>
     </Column>
   );
 };
