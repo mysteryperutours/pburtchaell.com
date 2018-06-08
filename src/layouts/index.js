a412363f5d217a4b4d579881c525b45965f22b1a
Patrick Burtchaell <patrick@pburtchaell.com>
2018-06-08 12:51:43 -0500

    Kill the baseline grid component with fire

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -9,14 +9,8 @@ import Baseline from '../components/Baseline';
  * Function: DefaultLayout
  * Description: Render the default layout used for all pages
  */
-const DefaultLayout = ({ children, location, ...restProps }) => (
-  <Baseline
-    lineHeight={9}
-    disabled={process.env.NODE_ENV !== 'development'}
-  >
-    {children({ ...restProps, location })}
-  </Baseline>
-);
+const DefaultLayout = ({ children, location, ...restProps }) => children({ ...restProps, location });
+
 
 DefaultLayout.propTypes = {
   children: PropTypes.func.isRequired,
